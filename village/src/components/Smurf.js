9f40cedb5e46caaffbc0571a8455f3e809c72990
Ash <ashley.cruz.crespo@gmail.com>
2019-05-17 11:23:29 -0400

    working on Smurf components

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Smurf = props => {
   return (
     <div className="Smurf">
-      <h3>{props.name}</h3>
+      <Link to ={`/smurfs/${props.id}`}><h3>{props.name}</h3></Link>
       <strong>{props.height} tall</strong>
       <p>{props.age} smurf years old</p>
+      <button className="dlt-btn" onCLick={e => props.deleteSmurf(e,props.id)}>X</button>
     </div>
   );
 };
